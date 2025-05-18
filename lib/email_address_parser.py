121c0a7d151a1ebe10c9644e8fdddfc37703e53a
Richard Wasonga <rowwasonga@gmail.com>
2025-05-18 14:05:05 +0300

    complete lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self,addresses):
+        self.addresses = addresses
+
+    def parse(self):
+
+        given_email = re.split(r'[,\s]+', self.addresses.strip())
+
+        email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.[a-zA-Z]+$')
+        filtered_emails = filter(email_pattern.fullmatch,given_email)
+
+        unique_address = sorted(set(filtered_emails))
+        return unique_address
